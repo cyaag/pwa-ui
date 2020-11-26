e7db268ce28fd2b910669fcca58b68e96b05647f
nalraajan <nalini.muthuraajan@gmail.com>
2020-11-26 13:58:24 +0530

    Shared data between components

--- src/components/LinearProgressBar.jsx
+++ src/components/LinearProgressBar.jsx
@@ -3,11 +3,18 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Line, Circle } from 'rc-progress';
 
+
 class LinearProgressBar extends Component {
-  constructor() {
-    super();
+
+  constructor(props) {
+    super(props);
+    console.log("Progress Bar ", JSON.stringify(props))
+    console.log("points - ", props.state.pointbudget)
+    console.log("rewards - ", props.state.rewardbudget)
+
     this.state = {
-      percent: 30,
+      rewardsPercent: parseInt(props.state.pointbudget),
+      pointsPercent: parseInt(props.state.rewardbudget),
       color: '#3FC7FA',
     };
     this.changeState = this.changeState.bind(this);
@@ -16,14 +23,14 @@ class LinearProgressBar extends Component {
   changeState() {
     const colorMap = ['#3FC7FA', '#85D262', '#FE8C6A'];
     const value = parseInt(Math.random() * 100, 10);
-    this.setState({
-      percent: value,
-      color: colorMap[parseInt(Math.random() * 3, 10)],
-    });
+    // this.setState({
+    //   percent: value,
+    //   color: colorMap[parseInt(Math.random() * 3, 10)],
+    // });
   }
 
   render() {
-    const { percent, color } = this.state;
+    const { rewardsPercent,pointsPercent, color } = this.state;
     const containerStyle = {
       width: '250px',
     };
@@ -34,18 +41,18 @@ class LinearProgressBar extends Component {
     };
     return (
       <div>
-        <h3>Line Progress {percent}%</h3>
+        <h3>Line Progress {rewardsPercent}%</h3>
         <div style={containerStyle}>
-          <Line percent={percent} strokeWidth="4" strokeColor={color} />
+          <Line percent={rewardsPercent} strokeWidth="4" strokeColor={color} />
           <Line
-            percent={[percent / 2, percent / 2]}
+            percent={[rewardsPercent / 2, rewardsPercent / 2]}
             strokeWidth="4"
             strokeColor={[color, '#CCC']}
           />
         </div>
-        <h3>Circle Progress {percent}%</h3>
+        <h3>Circle Progress {pointsPercent}%</h3>
         <div style={circleContainerStyle}>
-          <Circle percent={percent} strokeWidth="6" strokeLinecap="round" strokeColor={color} />
+          <Circle percent={pointsPercent} strokeWidth="6" strokeLinecap="round" strokeColor={color} />
         </div>
         <p>
           <button type="button" onClick={this.changeState}>
